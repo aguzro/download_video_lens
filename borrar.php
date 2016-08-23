90c1c9d2805f32b1a2c9d2d15406c04424db4b80
Allison Guzmán <bosquedeinvierno@outlook.com>
2016-08-22 23:08:43 -0300

    Arreglos varios

--- borrar.php
+++ borrar.php
@@ -1,11 +1,11 @@
-rescatar el nombtre del archivo con get
+rescatar el nombre del archivo con get
 
 tener el nombre del directorio y concatenarlo al archivo
 
 y hacerle unlick y eso linkearlo a la tabla 
 
 <?php  
-
+$_FILES[‘archivo’][‘name’] //nombre original del archivo
 
 
 
